4c3d973949e6fc68217fed7697cccd55a928b752
Danielle Costa <d.mariano.costa@gmail.com>
2025-08-27 19:01:01 -0300

    feat(db): corrige a criacao das tabelas do banco de dados

--- src/main/resources/db/changelog/migrations/db.changelog-202408210954.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202408210954.sql
@@ -4,12 +4,10 @@
 
 CREATE TABLE BOARDS_COLUMNS(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    `order` int NOT NULL,
-    kind VARCHAR(7) NOT NULL,
-    board_id BIGINT NOT NULL,
-    CONSTRAINT boards__boards_columns_fk FOREIGN KEY (board_id) REFERENCES BOARDS(id) ON DELETE CASCADE,
-    CONSTRAINT id_order_uk UNIQUE KEY unique_board_id_order (board_id, `order`)
-) ENGINE=InnoDB;
+        title VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        board_column_id BIGINT NOT NULL,
+        CONSTRAINT boards_columns__cards_fk FOREIGN KEY (board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE
+    ) ENGINE=InnoDB;
 
 --rollback DROP TABLE BOARDS_COLUMNS

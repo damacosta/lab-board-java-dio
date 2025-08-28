67e40d5931fbfa0a5c750748bfc3ec94f4dfbbd5
Danielle Costa <d.mariano.costa@gmail.com>
2025-08-28 17:31:36 -0300

    feat(service): Adicionar serviço para exibir detalhes do board

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -1,5 +1,6 @@
 package br.com.dio.persistence.dao;
 
+import br.com.dio.dto.BoardColumnDTO;
 import br.com.dio.persistence.entity.BoardColumnEntity;
 import com.mysql.cj.jdbc.StatementImpl;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static br.com.dio.persistence.entity.BoardColumnKindEnum.findByName;
 
@@ -18,28 +20,28 @@ public class BoardColumnDAO {
 
     public BoardColumnEntity insert(final BoardColumnEntity entity) throws SQLException {
         var sql = "INSERT INTO BOARDS_COLUMNS (name, `order`, kind, board_id) VALUES (?, ?, ?, ?);";
-        try(var statement = connection.prepareStatement(sql)){
+        try (var statement = connection.prepareStatement(sql)) {
             var i = 1;
-            statement.setString(i ++, entity.getName());
-            statement.setInt(i ++, entity.getOrder());
-            statement.setString(i ++, entity.getKind().name());
+            statement.setString(i++, entity.getName());
+            statement.setInt(i++, entity.getOrder());
+            statement.setString(i++, entity.getKind().name());
             statement.setLong(i, entity.getBoard().getId());
             statement.executeUpdate();
-            if (statement instanceof StatementImpl impl){
+            if (statement instanceof StatementImpl impl) {
                 entity.setId(impl.getLastInsertID());
             }
             return entity;
         }
     }
 
-    public List<BoardColumnEntity> findByBoardId(final Long id) throws SQLException{
+    public List<BoardColumnEntity> findByBoardId(final Long id) throws SQLException {
         List<BoardColumnEntity> entities = new ArrayList<>();
         var sql = "SELECT id, name, `order`, kind FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY `order`";
-        try(var statement = connection.prepareStatement(sql)){
+        try (var statement = connection.prepareStatement(sql)) {
             statement.setLong(1, id);
             statement.executeQuery();
             var resultSet = statement.getResultSet();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 var entity = new BoardColumnEntity();
                 entity.setId(resultSet.getLong("id"));
                 entity.setName(resultSet.getString("name"));
@@ -48,6 +50,37 @@ public class BoardColumnDAO {
                 entities.add(entity);
             }
             return entities;
+        }
+    }
+
+    public List<BoardColumnDTO> findByBoardIdWithDetails(final Long id) throws SQLException {
+        List<BoardColumnDTO> dtos = new ArrayList<>();
+        var sql =
+                """
+                        SELECT bc.id,
+                               bc.name,
+                               bc.kind,
+                               COUNT(SELECT c.id
+                                      FROM CARDS c
+                                      WHERE c.board_column_id = bc.id) cards_amount
+                          FROM BOARDS_COLUMNS bc
+                         WHERE board_id = ?
+                         ORDER BY `order`
+                        """;
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                var dto = new BoardColumnDTO(
+                        resultSet.getLong("bc.id"),
+                        resultSet.getString("bc.name"),
+                        findByName(resultSet.getString("bc.kind")),
+                        resultSet.getInt("cards_amount")
+                );
+                dtos.add(dto);
+            }
+            return dtos;
         }
     }
 }

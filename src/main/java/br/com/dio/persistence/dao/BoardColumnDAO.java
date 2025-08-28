5011f1b7465c8e51606841b5cb28538441d39c8c
Danielle Costa <d.mariano.costa@gmail.com>
2025-08-28 18:21:24 -0300

    feat(persistence): Implementar DAO para busca de cards por coluna

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -2,6 +2,7 @@ package br.com.dio.persistence.dao;
 
 import br.com.dio.dto.BoardColumnDTO;
 import br.com.dio.persistence.entity.BoardColumnEntity;
+import br.com.dio.persistence.entity.CardEntity;
 import com.mysql.cj.jdbc.StatementImpl;
 import lombok.RequiredArgsConstructor;
 
@@ -20,28 +21,28 @@ public class BoardColumnDAO {
 
     public BoardColumnEntity insert(final BoardColumnEntity entity) throws SQLException {
         var sql = "INSERT INTO BOARDS_COLUMNS (name, `order`, kind, board_id) VALUES (?, ?, ?, ?);";
-        try (var statement = connection.prepareStatement(sql)) {
+        try(var statement = connection.prepareStatement(sql)){
             var i = 1;
-            statement.setString(i++, entity.getName());
-            statement.setInt(i++, entity.getOrder());
-            statement.setString(i++, entity.getKind().name());
+            statement.setString(i ++, entity.getName());
+            statement.setInt(i ++, entity.getOrder());
+            statement.setString(i ++, entity.getKind().name());
             statement.setLong(i, entity.getBoard().getId());
             statement.executeUpdate();
-            if (statement instanceof StatementImpl impl) {
+            if (statement instanceof StatementImpl impl){
                 entity.setId(impl.getLastInsertID());
             }
             return entity;
         }
     }
 
-    public List<BoardColumnEntity> findByBoardId(final Long id) throws SQLException {
+    public List<BoardColumnEntity> findByBoardId(final Long boardId) throws SQLException{
         List<BoardColumnEntity> entities = new ArrayList<>();
         var sql = "SELECT id, name, `order`, kind FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY `order`";
-        try (var statement = connection.prepareStatement(sql)) {
-            statement.setLong(1, id);
+        try(var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, boardId);
             statement.executeQuery();
             var resultSet = statement.getResultSet();
-            while (resultSet.next()) {
+            while (resultSet.next()){
                 var entity = new BoardColumnEntity();
                 entity.setId(resultSet.getLong("id"));
                 entity.setName(resultSet.getString("name"));
@@ -53,25 +54,25 @@ public class BoardColumnDAO {
         }
     }
 
-    public List<BoardColumnDTO> findByBoardIdWithDetails(final Long id) throws SQLException {
+    public List<BoardColumnDTO> findByBoardIdWithDetails(final Long boardId) throws SQLException {
         List<BoardColumnDTO> dtos = new ArrayList<>();
         var sql =
                 """
-                        SELECT bc.id,
-                               bc.name,
-                               bc.kind,
-                               COUNT(SELECT c.id
-                                      FROM CARDS c
-                                      WHERE c.board_column_id = bc.id) cards_amount
-                          FROM BOARDS_COLUMNS bc
-                         WHERE board_id = ?
-                         ORDER BY `order`
-                        """;
-        try (var statement = connection.prepareStatement(sql)) {
-            statement.setLong(1, id);
+                SELECT bc.id,
+                       bc.name,
+                       bc.kind,
+                       COUNT(SELECT c.id
+                               FROM CARDS c
+                              WHERE c.board_column_id = bc.id) cards_amount
+                  FROM BOARDS_COLUMNS bc
+                 WHERE board_id = ?
+                 ORDER BY `order`
+                """;
+        try(var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, boardId);
             statement.executeQuery();
             var resultSet = statement.getResultSet();
-            while (resultSet.next()) {
+            while (resultSet.next()){
                 var dto = new BoardColumnDTO(
                         resultSet.getLong("bc.id"),
                         resultSet.getString("bc.name"),
@@ -83,4 +84,38 @@ public class BoardColumnDAO {
             return dtos;
         }
     }
+
+    public Optional<BoardColumnEntity> findById(final Long boardId) throws SQLException{
+        var sql =
+                """
+                SELECT bc.name,
+                       bc.kind,
+                       c.id,
+                       c.title,
+                       c.description
+                  FROM BOARDS_COLUMNS bc
+                 INNER JOIN CARDS c
+                    ON c.board_column_id = bc.id
+                 WHERE bc.id = ?;
+                """;
+        try(var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, boardId);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            if (resultSet.next()){
+                var entity = new BoardColumnEntity();
+                entity.setName(resultSet.getString("bc.name"));
+                entity.setKind(findByName(resultSet.getString("bc.kind")));
+                do {
+                    var card = new CardEntity();
+                    card.setId(resultSet.getLong("c.di"));
+                    card.setTitle(resultSet.getString("c.title"));
+                    card.setDescription(resultSet.getString("c.description"));
+                    entity.getCards().add(card);
+                }while (resultSet.next());
+            }
+            return Optional.empty();
+        }
+    }
+
 }
